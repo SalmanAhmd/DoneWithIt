5885ccf6285a065d0223d3dba31ca8f402623d0f
SalmanAhmd <salmanahmed1497@gmail.com>
2021-04-28 02:46:57 +0530

    adding chevron

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Image, StyleSheet, TouchableHighlight, View } from 'react-native'
-import colors from '../config/colors'
 import Swipeable from 'react-native-gesture-handler/Swipeable'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 import AppText from './AppText'
+import colors from '../config/colors'
 
 export default function ListItem({ title, subTitle, image, IconComponent, onPress, renderRightActions }) {
   return (
@@ -17,6 +18,7 @@ export default function ListItem({ title, subTitle, image, IconComponent, onPres
             <AppText style={styles.title}>{title}</AppText>
             {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
           </View>
+          <MaterialCommunityIcons color={colors.medium} name='chevron-right' size={25} />
         </View>
       </TouchableHighlight>
     </Swipeable>
@@ -26,10 +28,12 @@ export default function ListItem({ title, subTitle, image, IconComponent, onPres
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
+    alignItems: 'center',
     padding: 15,
     backgroundColor: colors.white
   },
   detailContainer: {
+    flex: 1,
     marginLeft: 10,
     justifyContent: 'center'
   },
