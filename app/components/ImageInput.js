d8663ad3110d2b7cf883dee61c035d489be47e72
SalmanAhmd <salmanahmed1497@gmail.com>
2021-05-03 03:01:10 +0530

    image input component

--- app/components/ImageInput.js
+++ app/components/ImageInput.js
@@ -1,18 +1,59 @@
-import React from 'react'
-import { Image, StyleSheet, View } from 'react-native'
+import React, { useEffect } from 'react'
+import { Alert, Image, StyleSheet, TouchableWithoutFeedback, View } from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker'
 
 import colors from '../config/colors'
 
-export default function ImageInput({ imageUri }) {
-  return (
-    <View style={styles.container}>
-      {imageUri ?
-        <Image source={{ uri: imageUri }} style={styles.image} />
-        :
-        <MaterialCommunityIcons color={colors.medium} name='camera' size={40} />
+export default function ImageInput({ imageUri, onChangeImage }) {
+
+  useEffect(() => {
+    requestPermission();
+  }, [])
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync()
+    if (!granted)
+      alert("You need to enable permission")
+  }
+
+  const hanlePress = () => {
+    if (!imageUri) {
+      selectImage()
+    }
+    else {
+      Alert.alert('Delete', 'Are you sure you want to delete this image?',
+        [{ text: 'Yes', onPress: () => onChangeImage(null) },
+        { text: 'No' }])
+    }
+  }
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 0.5
+      })
+      if (!result.cancelled) {
+        onChangeImage(result.uri)
       }
-    </View>
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={hanlePress} >
+      <View style={styles.container}>
+        {imageUri ?
+          <Image source={{ uri: imageUri }} style={styles.image} />
+          :
+          <MaterialCommunityIcons color={colors.medium} name='camera' size={40} />
+        }
+      </View>
+    </TouchableWithoutFeedback>
   )
 }
 
@@ -27,7 +68,7 @@ const styles = StyleSheet.create({
     overflow: 'hidden'
   },
   image: {
-    height: '100%',
-    width: '100%'
+    height: 100,
+    width: 100,
   }
 })
